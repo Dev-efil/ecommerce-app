09d79b0de9d516d5990c0d66be031c23bf9832df
Thanushkanth Shan <35372972+Dev-efil@users.noreply.github.com>
2024-03-17 18:04:28 -0400

    update: path & remove refreshToken middleware

--- server/routes/api-v1/wishList/wishListRoute.js
+++ server/routes/api-v1/wishList/wishListRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import { handleWishlist} from '../../../controllers/wishListController.js';
-import { verifyToken } from '../../../middleware/verifyToken.js';
-import { refreshToken } from '../../../middleware/refreshToken.js';
+import { verifyToken } from '../../../middlewares/verifyTokenMiddleware.js';
+// import { refreshToken } from '../../../middleware/refreshTokenMiddleware.js';
 
 
-router.get('/wishlist', verifyToken, refreshToken, handleWishlist);
+router.get('/wishlist', verifyToken, handleWishlist);
 
 
 export default router;
