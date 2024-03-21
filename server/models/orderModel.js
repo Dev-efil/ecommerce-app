9ab72b499b6dc1ac821e86fcb03b554b40c476e5
Thanushkanth Shan <35372972+Dev-efil@users.noreply.github.com>
2024-03-21 01:05:55 -0400

    update: order schemas

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,7 +1,23 @@
 import mongoose from 'mongoose';
 
+/* order */
 const orderSchema = new mongoose.Schema({
-/* ... */
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    orderDate: { type: Date, default: Date.now },
+    totalAmount: { type: Number },
 }, { timestamps: true });
 
-export default mongoose.model('orders', orderSchema);
+const order = mongoose.model('orders', orderSchema);
+
+
+/* order details */
+const orderDetailSchema = new mongoose.Schema({
+    orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'orders' },
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'products' },
+    totalAmount: { type: Number },
+}, { timestamps: true });
+
+const orderDetail = mongoose.model('orderDetails', orderDetailSchema);
+
+
+export { order, orderDetail };
