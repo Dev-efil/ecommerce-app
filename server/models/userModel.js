f0d0106d5f8637ecc046bf28a4612b579525f853
Thanushkanth Shan <35372972+Dev-efil@users.noreply.github.com>
2024-03-21 01:06:57 -0400

    update: user schemas

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,9 +1,33 @@
 import mongoose from 'mongoose';
 
+/* user */
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true}
+    password: { type: String, required: true },
+    phone: { type: String, default: null },
+    dateOfBirth: { type: Date, default: null },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    lastLoginDate: { type: Date }
 }, { timestamps: true });
 
-export default mongoose.model('users', userSchema);
+const user = mongoose.model('users', userSchema);
+
+
+/* address */
+const addressSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    addressLine1: { type: String },
+    addressLine2: { type: String },
+    city: { type: String },
+    state: { type: String },
+    postalCode: { type: String },
+    country: { type: String },
+    isBilling: { type: Boolean, default: false },
+    isShipping: { type: Boolean, default: false }
+}, { timestamps: true });
+
+const address = mongoose.model('addresses', addressSchema);
+
+
+export { user, address };
