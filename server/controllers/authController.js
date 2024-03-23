a2e91966638bf01cd882c35bd55d90fab000b3bd
Thanushkanth Shan <35372972+Dev-efil@users.noreply.github.com>
2024-03-23 08:37:49 -0400

    fix: refactoring authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,22 +1,23 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import User from '../models/userModel.js'
+import { user } from '../models/userModel.js'
 
 export const registerUserHandle = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-        const existingUser = await User.findOne({ email });
+        const existingUser = await user.findOne({ email });
         if (existingUser) {
             return res.sendStatus(400);
         }
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User({
+        const newuser = await user({
             name,
             email,
-            password: hashedPassword
+            password: hashedPassword,
+            lastLoginDate: new Date(),
         });
-        await user.save();
+        await newuser.save();
 
         return res.sendStatus(200);
     } catch (error) {
@@ -26,8 +27,10 @@ export const registerUserHandle = async (req, res) => {
 
 export const loginUserHandle = async (req, res) => {
     const { email, password } = req.body;
+    console.log(email,password);
     try {
-        const existingUser = await User.findOne({ email: email });
+        const existingUser = await user.findOne({ email: email });
+        console.log(existingUser);
         if (!existingUser) {
             return res.status(400).json({ message: 'User not found. Please register to login.' });
         }
@@ -35,10 +38,14 @@ export const loginUserHandle = async (req, res) => {
         if (!isPasswordValid) {
             return res.status(400).json({ message: 'Invalid email or password.' });
         }
+        console.log("here");
+        existingUser.lastLoginDate = new Date();
+        await existingUser.save();
         // const accessToken = await jwt.sign({ id: existingUser._id }, process.env.SECURE_ACCESS_KEY, { expiresIn: '22m' });
         const accessToken = await jwt.sign({ id: existingUser._id }, process.env.SECURE_ACCESS_KEY, { expiresIn: '35s' });
         // add secure = true in production 
         // res.cookie(String(existingUser._id), accessToken, { path: '/', expires: new Date(Date.now() + 1000 * 1800), httpOnly: true, sameSite: 'lax', secure: true });
+        console.log("here1");
         return res.cookie(String('prex' + existingUser._id), accessToken, { path: '/', expires: new Date(Date.now() + 1000 * 1200), httpOnly: true, sameSite: 'lax' }).status(200).json({ message: 'Successfully logged in.', accessToken });
     } catch (error) {
         return res.status(500).send(error);
@@ -48,7 +55,7 @@ export const loginUserHandle = async (req, res) => {
 export const getUserHandle = async (req, res) => {
     const { id } = req.params;
     try {
-        const existingUser = await User.findById(id);
+        const existingUser = await user.findById(id);
         if (!existingUser) {
             return res.status(400).json({ message: 'No user found.' });
         }
@@ -62,9 +69,9 @@ export const getUserHandle = async (req, res) => {
 
 export const updateUserHandle = async (req, res) => {
     const { id } = req.params;
-    const { email, name } = req.body;
+    const { name, phone, dateOfBirth } = req.body;
     try {
-        const existingUser = await User.updateOne({ id: id }, { $set: { name: name, email: email } });
+        const existingUser = await user.updateOne({ _id: id }, { $set: { name, phone, dateOfBirth } });
         if (!existingUser) {
             return res.status(400).json({ message: 'Update is failed.' });
         }
@@ -79,7 +86,7 @@ export const updateUserHandle = async (req, res) => {
 export const deleteUserHandle = async (req, res) => {
     const { id } = req.params;
     try {
-        const existingUser = await User.deleteOne({ _id: id });
+        const existingUser = await user.deleteOne({ _id: id });
         console.log('existingUser', existingUser);
         if (!existingUser) {
             return res.status(400).json({ message: 'User not found.' });
